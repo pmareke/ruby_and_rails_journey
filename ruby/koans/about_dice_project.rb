16b1412da4801e4cba2b310d82b846c3b2fce589
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2023-03-07 18:26:04 +0100

    feat: solve some koans

--- ruby/koans/about_dice_project.rb
+++ ruby/koans/about_dice_project.rb
@@ -1,10 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + "/neo")
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  attr_reader :values
+
+  def initialize
+    @values = []
+  end
+
+  def roll(number)
+    @values = (1..number).inject([]) { |acc, _| acc << Random.rand(1..6) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
--- ruby/koans/about_open_classes.rb
+++ ruby/koans/about_open_classes.rb
@@ -9,7 +9,7 @@ class AboutOpenClasses < Neo::Koan
 
   def test_as_defined_dogs_do_bark
     fido = Dog.new
-    assert_equal __, fido.bark
+    assert_equal "WOOF", fido.bark
   end
 
   # ------------------------------------------------------------------
@@ -23,8 +23,8 @@ class AboutOpenClasses < Neo::Koan
 
   def test_after_reopening_dogs_can_both_wag_and_bark
     fido = Dog.new
-    assert_equal __, fido.wag
-    assert_equal __, fido.bark
+    assert_equal "HAPPY", fido.wag
+    assert_equal "WOOF", fido.bark
   end
 
   # ------------------------------------------------------------------
@@ -36,8 +36,8 @@ class AboutOpenClasses < Neo::Koan
   end
 
   def test_even_existing_built_in_classes_can_be_reopened
-    assert_equal __, 1.answer_to_life_universe_and_everything?
-    assert_equal __, 42.answer_to_life_universe_and_everything?
+    assert_equal false, 1.answer_to_life_universe_and_everything?
+    assert_equal true, 42.answer_to_life_universe_and_everything?
   end
 
   # NOTE: To understand why we need the :: before Integer, you need to
