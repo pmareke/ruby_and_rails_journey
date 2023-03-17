869b127432b0d503b946f502116ba80d47b4596a
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2023-03-17 19:57:14 +0100

    feat: add game of life (WIP)

---
+++ ruby/books/4-design-rules/game-of-life/notes.md
@@ -0,0 +1,5 @@
+# [99 Bottles of OOP](https://sandimetz.com/99bottles)
+
+## Chapter 1: Rediscovering Simplicity
+
+- WIP
---
+++ ruby/books/4-design-rules/game-of-life/test/game_of_life_test.rb
@@ -0,0 +1,10 @@
+gem "minitest", "~> 5.4"
+require "minitest/autorun"
+require "minitest/pride"
+require_relative "../lib/game_of_life"
+
+class GameOfLifeTest < Minitest::Test
+  def test_XXX
+    assert 1 == 1
+  end
+end
