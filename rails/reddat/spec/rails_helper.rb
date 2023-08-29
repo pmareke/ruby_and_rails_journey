7f06e171eee2abc6c9e1e01264122acd674e5d69
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2023-08-29 22:27:26 +0200

    feat: show links in the root view

--- rails/reddat/spec/rails_helper.rb
+++ rails/reddat/spec/rails_helper.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
@@ -15,13 +16,11 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
 end
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
