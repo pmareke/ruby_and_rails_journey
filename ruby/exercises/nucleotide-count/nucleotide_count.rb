80fdc9f13511085c36faced4b6f86071cbe9707c
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2023-08-31 20:08:49 +0200

    feat: refaactor nucleotide

--- ruby/exercises/nucleotide-count/nucleotide_count.rb
+++ ruby/exercises/nucleotide-count/nucleotide_count.rb
@@ -1,17 +1,17 @@
 class Nucleotide
   attr_reader :histogram
-
   DEFAULT = {"A"=>0, "T"=>0, "C"=>0, "G"=>0}
+
+  def initialize(histogram)
+    @histogram = histogram
+  end
+
   def self.from_dna(dna)
     histogram = dna.chars.tally
 
     raise ArgumentError.new("INVALID") if _validate_histogram(histogram)
 
     new(DEFAULT.merge(histogram))
-  end
-
-  def initialize(histogram)
-    @histogram = histogram
   end
 
   def count(letter)
