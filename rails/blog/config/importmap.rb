b7c4451a341aec0f9c8ed29d586a84d9a3160172
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-02-12 09:02:53 +0100

    feat: add posts, simpleCSS, rich_text and JS localtime library

--- rails/blog/config/importmap.rb
+++ rails/blog/config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "local-time" # @3.0.2
